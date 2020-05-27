3af1ce9552d97bd16cd9450e81bc61d2f45dc647
brahim kehel <brahimkehel@gmail.com>
2020-05-27 21:15:51 +0000

    Approve

--- coreApi_PFA/Controllers/EtudiantsController.cs
+++ coreApi_PFA/Controllers/EtudiantsController.cs
@@ -23,7 +23,32 @@ namespace coreApi_PFA.Controllers
             _context = context;
         }
 
-        //GET: api/Enseignants/Nb
+        //GET: api/Etudiants/NonApp
+        [HttpGet]
+        [Route("NonApp")]
+        public async Task<object> GetNonApp()
+        {
+            return await (from e in _context.Etudiant
+                          join f in _context.Filiere on e.IdFiliere equals f.Id where e.Approve==false
+                          select new
+                          {
+                              Id = e.Id,
+                              Cin = e.Cin,
+                              DateNais = e.DateNais,
+                              Nom = e.Nom,
+                              Prenom = e.Prenom,
+                              Email = e.Email,
+                              MotdePasse = e.MotdePasse,
+                              Adresse = e.Adresse,
+                              Telephone = e.Telephone,
+                              Genre = e.Genre,
+                              CNE = e.Cne,
+                              IdFiliere = e.IdFiliere,
+                              Filiere = f.Libelle,
+                          }).ToListAsync();
+        }
+
+        //GET: api/Etudiants/Nb
         [HttpGet]
         [Route("Nb")]
         public async Task<int> GetNb()
@@ -35,7 +60,7 @@ namespace coreApi_PFA.Controllers
         [HttpGet]
         public async Task<object> GetEtudiant()
         {
-            return await (from e in _context.Etudiant join f in _context.Filiere on e.IdFiliere equals f.Id
+            return await (from e in _context.Etudiant join f in _context.Filiere on e.IdFiliere equals f.Id where e.Approve==true
                      select new
                      {   Id = e.Id,
                          Cin = e.Cin,
